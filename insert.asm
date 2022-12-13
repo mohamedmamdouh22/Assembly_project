bc68838d2a5e38603a1aabde2297f28907973ca4
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2022-12-14 01:48:20 +0200

    modified

--- insert.asm
+++ insert.asm
@@ -1,12 +1,11 @@
      
 data segment 
     
-arr dd 7dup('')
+arr DW 0  times 100
 
-msg db 'enter the number $',0ah,0dh  
+msg db 'enter the 8-digit number $',0ah,0dh  
  
 msg2 db 0ah,0dh, 'your number is $' 
-
 data ends
 
 code segment
@@ -21,16 +20,28 @@ code segment
          
          
       
-        
+       mov  si,offset arr     ;CX will point to the current element to insert in
        loop1:
              mov ah,1
-             int 21h 
-             mov bl,al
-             shl bl,4
-             add al,bl
-             cmp al,'a'
+             int 21h
+             mov [si],al
+             inc si
+             cmp al,65h
              JNZ loop1 
-                       
+             mov cx,8
+             mov si,offset arr
+             
+             mov ah,9
+             lea dx,msg2
+             int 21h
+             
+             
+             loop2:
+             mov dl,[si]
+             mov ah,2
+             int 21h
+             inc si
+             loop loop2          
          
              
            
