40627e5ece407bf35ec50abd49f14ba3dbf2a88b
Yasmin-attia <eng.yasmen.attia@gmail.com>
2022-12-15 00:25:23 +0200

    - create file display for make loop2 at messages

---
+++ display.asm
@@ -0,0 +1,44 @@
+data segment
+
+   msg2 db 0ah,0dh, 'name is $'
+   len1 equ $-msg2
+
+   msg3 db 0ah,0dh, 'number is $' 
+   len2 equ $-msg3
+
+
+data ends
+
+code segment
+
+    assume cs:code, ds:data
+    main proc far
+        mov ax, data
+        mov ds, ax
+
+        mov cx, len1
+        mov ah, 2
+        mov bx, offset msg2
+
+        a:
+            mov dl, [bx]
+            int 21h
+            inc bx
+        loop a
+
+         mov cx, len2
+         mov ah, 2
+         mov bx, offset msg3 
+       
+       b:
+            mov dl, [bx]
+            int 21h
+            inc bx
+        loop b 
+
+        
+        mov ah, 4ch
+        int 21h
+    main endp
+code ends
+end main
