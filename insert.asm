a7c3424f2f455351eff239fd1909a648aef2489f
mohamedmamdouh22 <muhammed195896@feng.bu.edu.eg>
2022-12-14 18:39:22 +0200

    insert modified

--- insert.asm
+++ insert.asm
@@ -1,11 +1,15 @@
      
 data segment 
     
-arr DW 0  times 100
+names DD 0  times 100
+numbers DD 0  times 100
 
-msg db 'enter the 8-digit number $',0ah,0dh  
+msg db 'enter the name and enter 0 to terminate: $',0ah,0dh
+msg1 db 'enter the 11-digit  number and enter e to terminate: $',0ah,0dh  
  
-msg2 db 0ah,0dh, 'your number is $' 
+msg2 db 0ah,0dh, 'name is $'
+msg3 db 0ah,0dh, 'number is $' 
+n_line DB 0AH,0DH,"$"   ;for new line
 data ends
 
 code segment
@@ -17,32 +21,61 @@ code segment
          mov ah,9
          lea dx,msg
          int 21h     
-         
-         
-      
-       mov  si,offset arr     ;CX will point to the current element to insert in
+       mov  si,offset names     ;CX will point to the current element to insert in  
+       ; inser name
        loop1:
              mov ah,1
              int 21h
              mov [si],al
              inc si
-             cmp al,65h
-             JNZ loop1 
-             mov cx,8
-             mov si,offset arr
-             
-             mov ah,9
-             lea dx,msg2
+             cmp al,30h
+             JNZ loop1
+          mov [si],'h'    
+          mov ah,9
+          lea dx,n_line
+          int 21h
+         ;to print the msg
+         mov ah,9
+         lea dx,msg1
+         int 21h
+         mov si,offset numbers
+         inc si
+        ; insert number  
+       loop2:
+             mov ah,1
              int 21h
-             
-             
-             loop2:
+             mov [si],al
+             inc si
+             cmp al,65h
+             JNZ loop2
+       mov cx,5
+       mov si,offset names
+       mov ah,9
+       lea dx,msg2
+       int 21h
+       ; print name
+       loop3:
              mov dl,[si]
              mov ah,2
              int 21h
              inc si
-             loop loop2          
-         
+             loop loop3 
+       mov cx,11
+       mov si,offset numbers
+       inc si
+       mov ah,9
+       lea dx,msg3
+       int 21h
+       ;print number
+       loop4:
+             mov dl,[si]
+             mov ah,2
+             int 21h
+             inc si
+             loop loop4          
+          
+                       
+           
              
            
                  
