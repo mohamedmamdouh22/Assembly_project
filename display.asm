79c29cc7405e5892b466833d5c11bc46b798ad57
yasmenattia <yasmin180951@feng.bu.edu.eg>
2022-12-16 05:31:50 +0200

    loop

--- display.asm
+++ display.asm
@@ -1,47 +1,29 @@
 data segment
-
-   msg2 db 0ah,0dh, 'name is $'
-   len1 equ $-msg2
-
-   msg3 db 0ah,0dh, 'number is $' 
-   len2 equ $-msg3
-
-
+    msg2 db 'yasmen'
+    len1 equ $-msg2
+    msg3 db '112000'
+    len2 equ $-msg3
 data ends
-
 code segment
-
-
+    assume cs:code,ds:data
     main proc far
-        mov ax, data
-        mov ds, ax
-
-        
-
-        mov ah, 2
-        mov bx, offset msg2
-
-
+        mov ax,data
+        mov ds,ax
+        mov cx,len1
+        mov ah,2
+        mov bx,offset msg2
+        a:
+           mov dl,[bx]
+           int 21h
+           inc bx
+        loop a 
        
-
-
-        a:   
-            mov dl, [bx]
-            int 21h
-            inc bx
-        loop a
-
-
-         mov ah, 2
-         mov bx, offset msg3 
-       
-       b:
-            mov dl, [bx]
-            int 21h
-            inc bx
-        loop b 
-
-
+        b:
+           mov dl,[bx]
+           int 21h
+           inc bx
+        loop b
+        mov ah ,4ch
         int 21h
     main endp
 code ends
