0b2ab3517467b1dc067529bf36f6b0fac0a6da95
yasmenattia <yasmin180951@feng.bu.edu.eg>
2022-12-20 18:46:13 +0200

    display

--- phoneBook_test.asm
+++ phoneBook_test.asm
@@ -129,7 +129,33 @@ JE START
      
      
      DISPLAY:
-      
+        mov si,offset names
+        mov bx,offset numbers
+        mov cx,counter
+        outerLoop:
+            mov ah,9h
+            mov dx,offset nameP
+            int 21h
+            loop7:
+                mov ah,2
+                mov dx,[si]
+                int 21h
+                inc si
+                cmp [si],'$'
+                jnz loop7
+                inc si
+            loop8:
+                mov ah,2
+                mov dx,[bx]
+                int 21h
+                inc bx
+                cmp [bx],'$'
+                jnz loop8
+                inc bx     
+            mov ah,9h
+            mov dx,offset n_line
+            int 21h
+            loop outerLoop
             
             ;FOR CONTINUE
             MOV AH,09H
